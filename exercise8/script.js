f3694b8ab6e0be2a1f5e7803d2787793e4bb1938
Stefano Candiani <stefanocandiani1@MacBook-Air-di-Stefano.local>
2020-11-19 10:55:55 -0500

    11/18/2020

---
+++ exercise8/script.js
@@ -0,0 +1,32 @@
+// Create an object and an array which we will use in our facebook exercise. 
+
+// 1. Create an object that has properties "username" and "password". Fill those values in with strings.
+
+let user = {
+    username:"Bobby",
+    password: "Bobby123"
+}
+
+// 2. Create an array which contains the object you have made above and name the array "database".
+
+let database = [user];
+
+// 3. Create an array called "newsfeed" which contains 3 objects with properties "username" and "timeline".
+
+let newsfeed = [
+    {
+        username: "Stefano",
+        timeline: "Nov 12 2020"
+    },
+
+    {
+        username: "Robert",
+        timeline: "Jan 15 2019"
+    },
+
+    {
+        username: "Fief Lord Emmanuel",
+        timeline: "Aug 20th 1845"
+
+    }
+]
